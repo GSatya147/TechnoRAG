cd0180fba4e069953d6290bb95b6065769efbde9
GSatya147 <gvvsn147@gmail.com>
2026-06-29 20:35:07 +0100

    code: add config dev constants

--- configurables/config.py
+++ configurables/config.py
@@ -1,7 +1,19 @@
 import os
+import logging
+import sys
+
 from dotenv import load_dotenv
 
 load_dotenv()
+
+logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s | %(levelname)-8s | %(name)s | %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        handlers=[logging.StreamHandler(sys.stdout)],
+    )
+
+logger = logging.getLogger(__name__)
 
 # Database
 DB_HOST                 = os.getenv("DB_HOST")
@@ -21,3 +33,10 @@ DEEPSEEK_MODEL          = "deepseek-v4-flash"
 LANGFUSE_PUBLIC_KEY     = os.getenv("LANGFUSE_PUBLIC_KEY")
 LANGFUSE_SECRET_KEY     = os.getenv("LANGFUSE_SECRET_KEY")
 LANGFUSE_BASE_URL       = os.getenv("LANGFUSE_BASE_URL")
+
+# Consts
+MAX_ARTICLES            = 600
+MAX_ARTICLES_TESTING    = 2
+CHUNK_SIZE              = 512
+CHUNK_OVERLAP           = int(0.15 * CHUNK_SIZE)
+BATCH_SIZE              = 5000
---
+++ ingestion/embedder.py
@@ -0,0 +1,7 @@
+from sentence_transformers import SentenceTransformer
+
+from configurables.config import VOYAGE_API_KEY, BATCH_SIZE
+
+class Embedder:
+    def __init__(self):
+        pass
