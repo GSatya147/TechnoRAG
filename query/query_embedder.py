9bc71959e26fc46a3b1a1735dadc9a65674a2d35
GSatya147 <gvvsn147@gmail.com>
2026-07-02 00:50:04 +0100

    code: add query embedder - llama way

--- query/query_embedder.py
+++ query/query_embedder.py
@@ -5,7 +5,6 @@ from configurables.config import VOYAGE_API_KEY, VOYAGE_EMBEDDING_MODEL, logger
 def query_embedder(user_query: str):
     try:
         vo_client = voyageai.Client(api_key=VOYAGE_API_KEY)
-
         embeddings = vo_client.embed(texts=user_query, output_dimension=1024, model=VOYAGE_EMBEDDING_MODEL).embeddings
 
         return embeddings[0]
