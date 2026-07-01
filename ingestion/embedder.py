9e423565cb509adfea4b2e874ac91d62c00d4483
GSatya147 <gvvsn147@gmail.com>
2026-07-01 15:37:05 +0100

    code: add embedder and node modifier

--- ingestion/embedder.py
+++ ingestion/embedder.py
@@ -3,8 +3,7 @@ import voyageai
 from configurables.config import VOYAGE_API_KEY, BATCH_SIZE, VOYAGE_EMBEDDING_MODEL, logger
 
 class Embedder:
-    def __init__(self, chunk_dict_list: list[dict]):
-        self.chunks_dict: list[dict] = chunk_dict_list
+    def __init__(self):
 
         try:
             self.vo_client = voyageai.Client(api_key=VOYAGE_API_KEY)
@@ -12,13 +11,13 @@ class Embedder:
         except Exception as e:
             logger.error(e)
 
-    def article_embedder(self):
+    def article_embedder(self, chunks_dict: list[dict]):
         try:
             embeddings = []
 
             running_tokens = 0
             current_batch = []
-            for chunk in self.chunks_dict:
+            for chunk in chunks_dict:
                 if chunk["tokens_count"] + running_tokens >= BATCH_SIZE:
                     embeddings+=self.vo_client.embed(texts=current_batch, model=VOYAGE_EMBEDDING_MODEL, output_dimension=1024).embeddings
                     running_tokens = 0
@@ -35,10 +34,14 @@ class Embedder:
         except Exception as e:
             logger.error(e)
 
-    def embed_and_modify(self):
-        embeddings = self.article_embedder()
-        for chunk, embedding in zip(self.chunks_dict, embeddings):
-            chunk["embedding"] = embedding
+    def embed_and_modify(self, chunks_dict: list[dict]):
+        embeddings = self.article_embedder(chunks_dict=chunks_dict)
 
-        return self.chunks_dict
+        if embeddings:
+            for chunk, embedding in zip(chunks_dict, embeddings):
+                chunk["embedding"] = embedding
+
+            return chunks_dict
+        
+        return None
 
